93cfc177a784c03d86f29afdc53048a5cf8b5533
Kaê Renato Brazauskas <brazakae@gmail.com>
2025-01-26 00:47:22 -0300

    feat: criado o TodoService e feito a logica da listagem

---
+++ src/main/java/com/kae/to_do_list/service/TodoService.java
@@ -0,0 +1,35 @@
+package com.kae.to_do_list.service;
+
+import com.kae.to_do_list.entity.Todo;
+import com.kae.to_do_list.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> list(){
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+    public List<Todo> update(){
+        return list();
+    }
+    public List<Todo> delete(){
+        return list();
+    }
+}
